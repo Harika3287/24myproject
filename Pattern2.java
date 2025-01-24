9c87e0f0dc98c295d448858902f31c2530f39a29
Harika3287 <22bk1a05g4@stpetershyd.com>
2025-01-24 14:45:54 +0530

    first commit

---
+++ Pattern2.java
@@ -0,0 +1,17 @@
+package Sample;
+
+public class Pattern2 {
+
+	public static void main(String[] args) {
+		for(int c=5;c>=1;c--) { //
+			for(int r=5;r>=c;r--) { //
+				System.out.print(c+" "); //
+			}
+			System.out.println();
+		}
+	}
+}
+
+	
+
+
